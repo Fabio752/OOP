f3ff02f21a699db409e7b545dc77149003d095fe
Fabio Deo <36317183+Fabio752@users.noreply.github.com>
2018-10-19 23:51:37 +0100

    Update Point.java

--- JAVA/Triangle/Point.java
+++ JAVA/Triangle/Point.java
@@ -2,25 +2,25 @@
 import java.lang.Math;
 
 public class Point {
-		private double x_;
-		private double y_;
-		private double distance_;
-		
-		Point(double x, double y){
-			x_ = x;
-			y_ = y;
-			distance_ = Math.sqrt(x * x + y * y);
-		}
-		
-		public double GetX() {
-			return x_;
-		}
-		public double GetY() {
-			return y_;
-		}
-		public double GetDistanceFromOrigin() {
-			return distance_;
-		}
+	private double x_;
+	private double y_;
+	private double distance_;
+
+	Point(double x, double y){
+		x_ = x;
+		y_ = y;
+		distance_ = Math.sqrt(x * x + y * y);
+	}
+
+	public double GetX() {
+		return x_;
+	}
+	public double GetY() {
+		return y_;
+	}
+	public double GetDistanceFromOrigin() {
+		return distance_;
+	}
         
         public void SetX(double x_in) {
         	x_ = x_in;
@@ -49,19 +49,19 @@ public class Point {
         public void TranslatePoint(Point translator_point) {
         	x_ = x_ + translator_point.x_;
         	y_ = y_ + translator_point.y_;
-		}
+	}
 
-		//Operators
-		public boolean GreaterThan(Point other_point){
-			return distance_ > other_point.GetDistanceFromOrigin();
-		}
+	//Operators
+	public boolean GreaterThan(Point other_point){
+		return distance_ > other_point.GetDistanceFromOrigin();
+	}
 
-		public boolean SmallerThan(Point other_point){
-			return distance_ < other_point.GetDistanceFromOrigin();
-		}
-		public boolean EqualTo(Point other_point){
-			return (x_ == other_point.GetX()) && (y_ == other_point.GetY());
-		}
+	public boolean SmallerThan(Point other_point){
+		return distance_ < other_point.GetDistanceFromOrigin();
+	}
+	public boolean EqualTo(Point other_point){
+		return (x_ == other_point.GetX()) && (y_ == other_point.GetY());
+	}
 		
 
         
